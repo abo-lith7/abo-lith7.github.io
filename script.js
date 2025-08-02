edf89768d791197f7f1dfc1f76bf6091a37fb373
abo-lith7 <saeedalsnwy78@gmail.com>
2025-08-02 03:09:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js - تحريك فقاعات آراء الجمهور من اليمين لليسار
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".testimonial-bubbles");
-  if (container) {
-    container.style.animation = "scroll-left 25s linear infinite";
-  }
-});
